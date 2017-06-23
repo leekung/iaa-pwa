d58bcaac7be43e35fd0d41c0fdfdb46f8c6e34c9
Leekung <grmark@gmail.com>
2017-06-23 20:07:01 +0700

    init Yo

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyAr3D93-2wFwX24ps9dpdsUYPt_WTO5OHk",
+    authDomain: "leekung-d70fa.firebaseapp.com",
+    databaseURL: "https://leekung-d70fa.firebaseio.com/",
+    storageBucket: "gs://leekung-d70fa.appspot.com/",
+    messagingSenderId: "199678915818"
+};
+
